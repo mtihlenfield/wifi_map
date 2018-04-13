93c46cc0f696de2de1bb241a9bbffd9ab1b7022c
Matt Ihlenfield <mtihlenfield@protonmail.com>
2018-04-13 00:13:53 -0400

    project structure changes

---
+++ wifi_map/wifi_map.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+
+def main():
+    # TODO parse/validate args
+    # TODO verify rabbitmq is running
+    # TODO initialize database
+    # TODO spin up server process
+    # TODO run reader
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
